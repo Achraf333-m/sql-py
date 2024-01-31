df2f152c554c3795d39bcb9b88cd7e3c0b01164e
Achraf333-m <achrafdaimallah2003@gmail.com>
2024-01-31 15:55:10 -0500

    final solution

--- email_count.py
+++ email_count.py
@@ -5,26 +5,27 @@ curs = conx.cursor()
 
 curs.execute('DROP TABLE IF EXISTS Counts')
 
-curs.execute('CREATE TABLE Counts (email TEXT, count INTEGER)')
+curs.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
 
 file = input('Enter file name:')
 # if len(file) < 1: print('ERROR, FILE NOT FOUND')
-fh = open(file).read().split('\n')
+fh = open(file).readlines()
 
 for line in fh:
     if not line.startswith('From: '): continue
-    pieces = line.split()
-    email = pieces[1]
-    curs.execute('SELECT count FROM Counts WHERE email = ?', (email,))
+    org = line.split('@')[1].strip()
+
+
+    curs.execute('SELECT count FROM Counts WHERE org = ?', (org,))
     row = curs.fetchone()
 
     if row is None:
-        curs.execute(''' INSERT INTO Counts (email, count) VALUES (?, 1)''', (email,))
+        curs.execute(''' INSERT INTO Counts (org, count) VALUES (?, 1)''', (org,))
     else:
-        curs.execute('UPDATE Counts SET count = count + 1 WHERE email = ?', (email,))
+        curs.execute('UPDATE Counts SET count = count + 1 WHERE org = ?', (org,))
     
     conx.commit()
-sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 for row in curs.execute(sqlstr):
     print(row[0], row[1])
